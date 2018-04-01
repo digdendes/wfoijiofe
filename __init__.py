1f5ae771bab0ac13c430784d9c0f9405b2c65fed
patmo141 <patrick.moore.bu@gmail.com>
2018-04-01 11:54:41 -0400

    v0.4.2 tag

--- __init__.py
+++ __init__.py
@@ -28,7 +28,7 @@
 bl_info = {
     'name': "D3T Splint Module",
     'author': "Patrick R. Moore",
-    'version': (0,4,1),
+    'version': (0,4,2),
     'blender': (2, 7, 9),
     'api': "3c04373",
     'location': "3D View -> Tool Shelf",
